18422e0eddd8e4cb7721545bdaded01efa6d35a8
woohm402 <woohm404@gmail.com>
2025-03-06 15:45:46 +0900

    add reproduction

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div className="text-Title_Strong">text-Title_Strong</div>
+    <div className="text-Title1_Strong">text-Title1_Strong</div>
   </React.StrictMode>,
-)
+);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      Title_Strong: ["24px", { lineHeight: "32px", fontWeight: "700" }],
+      Title1_Strong: ["24px", { lineHeight: "32px", fontWeight: "700" }],
+    },
+  },
+};
